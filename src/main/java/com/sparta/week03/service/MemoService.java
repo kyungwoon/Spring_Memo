cab07a09287f914dfea5141a43a4fe9a8b9c72b2
kyungwoon <nakw0410@gmail.com>
2022-01-23 11:30:43 +0900

    feat: Created Service(update)

---
+++ src/main/java/com/sparta/week03/service/MemoService.java
@@ -0,0 +1,26 @@
+package com.sparta.week03.service;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.domain.MemoRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor // 기본 생성
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional // 실제 DB에 반영이 되어야 한다느
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
+}
+
